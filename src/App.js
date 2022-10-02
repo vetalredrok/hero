93e7add430308f57e7a06be7088b38e5ea95737a
vetalredrok <vetalredrok@gmail.com>
2022-10-02 19:17:22 +0300

    app class

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component{
+  constructor() {
+    super();
 
-    </div>
-  );
+    this.state = {
+      name: 'Yihua'
+    };
+  }
+
+
+
+  render() {
+    return (
+        <div className="App">
+        <p>Hi {this.state.name} </p>
+          <button onClick={()=>{
+            this.setState({name: 'Andrei'})
+          }}>Change Name</button>
+        </div>
+    );
+
+
+  }
 }
 
 export default App;
