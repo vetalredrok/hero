1f01e5a264f4ce983f9f1620439bd7f8b71c74fd
vetalredrok <vetalredrok@gmail.com>
2022-10-03 13:45:31 +0300

    add first page

---
+++ src/components/category-item/category-item.js
@@ -0,0 +1,19 @@
+import './category-item.scss'
+import React from 'react';
+
+const CategoryItem = ({category:{imageUrl, title}}) => {
+
+    return (
+        <div className='category-container'>
+            <div className='background-image' style={{
+                backgroundImage:`url(${imageUrl})`}}/>
+            <div className='category-body-container'>
+                <h2>{title}</h2>
+                <p>Shop Now</p>
+
+            </div>
+        </div>
+    );
+};
+
+export {CategoryItem};
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export * from './category-item/category-item'
+
+export * from './directory/directory.component'
